c4dc53e3dc8e99881e38696bde3bdb4e83e2b9cb
김남태 <ryan@thebiz.kr>
2023-06-01 09:40:51 +0900

    responses에 default형태로 exception format 지정

--- app/exception/exception_handler.py
+++ app/exception/exception_handler.py
@@ -9,25 +9,26 @@ async def http_exception_handler(request: Request, exc: HTTPException):
         status_code=exc.status_code,
         content={
             "code": exc.status_code,
-            "message": str(exc.detail)
+            "message": str(exc.detail),
+            "data": None
         }
     )
 
 
 async def request_error_handler(request: Request, exc: RequestValidationError):
     exc_json = jsonable_encoder(exc.errors())
-    response = {"message": "", "data": None}
+    response = {"code": 422, "message": "", "data": None}
     for error in exc_json:
         # response['message'].append(error['loc'][-1]+f": {error['msg']}")
-        response['message'] = error['loc'][-1] + f" validate error"
+        response['message'] = error['loc'][-1] + f"의 값이 잘못되었습니다"
 
     return JSONResponse(response, status_code=422)
 
 
 async def validation_error_handler(request: Request, exc: ValidationError):
     exc_json = exc.errors()
-    response = {"message": "", "data": None}
+    response = {"code": 400, "message": "", "data": None}
     for error in exc_json:
         response['message'] = error['msg']
 
-    return JSONResponse(response, status_code=422)
+    return JSONResponse(response, status_code=400)
--- app/main.py
+++ app/main.py
@@ -6,7 +6,31 @@ from app.exception import exception_handler as ex
 app = FastAPI(
     title="API",
     version="0.1",
-    description=""
+    description="",
+    responses={
+        400: {
+            "content": {
+                "application/json": {
+                    "example": {
+                        "code": 400,
+                        "message": "request error",
+                        "data": None
+                    }
+                }
+            }
+        },
+        422: {
+            "content": {
+                "application/json": {
+                    "example": {
+                        "code": 422,
+                        "message": "${field}의 값이 잘못되었습니다",
+                        "data": None
+                    }
+                }
+            }
+        }
+    }
 )
 
 # exception handler
